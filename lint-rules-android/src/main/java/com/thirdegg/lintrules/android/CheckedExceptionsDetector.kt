0a16439caa143c62b934a39095c626ba198a18f2
LineCutFeng <932413237@qq.com>
2019-10-06 16:40:33 +0800

    Update CheckedExceptionsDetector.kt

--- lint-rules-android/src/main/java/com/thirdegg/lintrules/android/CheckedExceptionsDetector.kt
+++ lint-rules-android/src/main/java/com/thirdegg/lintrules/android/CheckedExceptionsDetector.kt
@@ -16,7 +16,7 @@ val ISSUE_PATTERN = Issue.create("CheckedExceptions",
         "Checked exceptions for kotlin",
         "Checked exceptions for kotlin",
         CORRECTNESS,
-        1,
+        10,
         Severity.ERROR,
 //        WARNING,
         Implementation(CheckedExceptionsDetector::class.java, EnumSet.of(Scope.JAVA_FILE))
